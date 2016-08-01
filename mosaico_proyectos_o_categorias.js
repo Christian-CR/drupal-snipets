a6729150db5ff966bd4fe8735c26319e7d05b5b7
vodoo <VodooPeople@users.noreply.github.com>
2016-08-01 17:02:47 -0600

    se comentó el código

--- mosaico_proyectos_o_categorias.js
+++ mosaico_proyectos_o_categorias.js
@@ -4,9 +4,9 @@ jQuery(document).ready(function(){
 	// selecciona el proyecto y esconde el resto
 	function selectorProyectos() {
 		jQuery(".selector-proyectos div").click(function(){
-			var ecat = jQuery(this).attr('id');
-			var tpi = jQuery(".itemProyecto").hasClass(ecat);
-			if(tpi === true){
+			var ecat = jQuery(this).attr('id'); // obtiene el id del boton
+			var tpi = jQuery(".itemProyecto").hasClass(ecat); //compara si la existe una clase que se llame igual
+			if(tpi === true){ // si es verdad que existe la clase, esconde las demás y muestra la seleccionada
 				jQuery(".itemProyecto").hide();
 				jQuery("."+ecat).delay().show();
 				console.log('si sirve');
