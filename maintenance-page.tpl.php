c25037fd9265415811a8be78410bb8af04228f0d
vodoo <VodooPeople@users.noreply.github.com>
2018-02-20 09:11:38 -0600

    archivo tpl de mantenimiento personalizado

---
+++ maintenance-page.tpl.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Almacen Mauro</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style type="text/css">
+.movil{
+	display: none;
+}
+
+@media only screen and (max-width: 480px){
+
+	.desktop{
+		display: none!important;
+	}
+	.movil{
+	display: block!important;
+	}
+}
+
+</style>
+</head>
+<body>
+<div style="text-align: center" class="desktop">
+<img src="sites/default/files/mantenimiento2.jpg" alt="" style="margin: 0 auto; width: 100%; max-width: 1200px; height: auto;">
+</div>
+
+<div class="movil">
+	<img src="sites/default/files/mant-movil.jpg" alt="" style="margin: 0 auto; width: 100%; max-width: 480px; height: auto;">
+</div>
+</body>
+</html>
